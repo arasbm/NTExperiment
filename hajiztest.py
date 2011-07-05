9017124895bf149f0ce8191521b2b4902fa4bba7
Amir Hossein Hajizadeh <amirhos.hajiz@gmail.com>
2011-07-04 20:53:39 -0700

    adding content to hajiz test

---
+++ hajiztest.py
@@ -0,0 +1,66 @@
+import kivy
+kivy.require('1.0.6')
+
+from random import random, randint
+
+from kivy.app import App
+
+from kivy.uix.widget import Widget
+from kivy.uix.scatter import Scatter
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.slider import Slider
+from kivy.uix.image import Image
+
+from kivy.properties import StringProperty
+
+from kivy.graphics import Line, Ellipse, Color
+
+class MyWidget(Widget):
+	def on_touch_down(self, touch):
+		with self.canvas:
+			Color (1,0,0)
+			Ellipse(pos=(touch.x-5, touch.y-5), size=(10, 10))
+			Color (*(random(),random(),random()))
+			touch.ud['harchi'] = Line(points=(touch.x, touch.y, touch.y, touch.x))
+
+	def on_touch_move(self, touch):
+		if 'harchi' in touch.ud:
+			touch.ud['harchi'].points += [touch.x, touch.y, touch.y, touch.x]
+
+	def on_touch_up(self, touch):
+		with self.canvas:
+			Color (0,0,1)
+			Ellipse(pos=(touch.x-5, touch.y-5), size=(10, 10))
+
+
+class TouchtracerApp(App):
+	def build(self):
+		root = Widget()
+
+		# trying custom widget
+		painter = MyWidget()
+		with painter.canvas:
+			Color (1,1,1)
+			Line(points=(100,100,2000,2000))
+		root.add_widget(painter)
+
+		# trying button
+		clrbtn = Button(text='Reset')
+		root.add_widget(clrbtn)
+		def reset_canvas(obj):
+			painter.canvas.clear()
+			with painter.canvas:
+				Color (1,1,1)
+				Line(points=(100,100,2000,2000))
+		clrbtn.bind(on_release=reset_canvas)
+
+		# trying scatter 
+		picture = Scatter(rotation=randint(-30,30))
+		picture.add_widget(Image(source='dome.jpg'))
+		root.add_widget(picture)
+
+		return root
+
+if __name__ in ('__main__', '__android__'):
+	TouchtracerApp().run()
