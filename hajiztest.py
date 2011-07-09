a6ef3c536fc2d8a9a8920732782be53644cf26c7
Amir Hossein Hajizadeh <amirhos.hajiz@gmail.com>
2011-07-09 14:05:37 -0700

    enhancing class structures

--- hajiztest.py
+++ hajiztest.py
@@ -9,9 +9,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 from kivy.uix.image import Image
+from kivy.uix.scrollview import ScrollView
 
 from kivy.animation import Animation
 
@@ -20,12 +22,31 @@ from kivy.properties import StringProperty
 from kivy.graphics import Line, Ellipse, Color
 
 class MyWidget(Widget):
+	def __init__(self):
+		Widget.__init__(self)		
+		with self.canvas:
+			Color (1,1,1)
+			Line(points=(100,100,2000,2000))
+		clrbtn = Button(text='Reset')
+		def reset_canvas(obj):
+			children = self.children[:]
+			self.canvas.clear()
+			with self.canvas:
+				Color (1,1,1)
+				Line(points=(100,100,2000,2000))
+			for child in children:
+				self.add_widget(child)
+		clrbtn.bind(on_release=reset_canvas)
+		self.add_widget(clrbtn)
+
 	def on_touch_down(self, touch):
 		with self.canvas:
 			Color (1,0,0)
 			Ellipse(pos=(touch.x-5, touch.y-5), size=(10, 10))
 			Color (*(random(),random(),random()))
 			touch.ud['whatever'] = Line(points=(touch.x, touch.y, touch.y, touch.x))
+		for child in self.children[:]:
+			child.dispatch('on_touch_down', touch)
 
 	def on_touch_move(self, touch):
 		if 'whatever' in touch.ud:
@@ -37,42 +58,41 @@ class MyWidget(Widget):
 				Color (0,0,1)
 				Ellipse(pos=(touch.x-5, touch.y-5), size=(10, 10))
 
+class Window(Scatter):
+	def __init__(self):
+		Scatter.__init__(self, pos=(200,200), size=(300,300))
+		layout = BoxLayout(pos=(50, 50), size=(200, 200))
+		btn1 = Button(text='Hello')
+		btn2 = Button(text='World')
+		layout.add_widget(btn1)
+		layout.add_widget(btn2)
+		self.add_widget(Image(source='img/background.jpg', size=(300,300)))
+		self.add_widget(layout)
+
+class Workspace(ScrollView):
+	def __init__(self):
+		ScrollView.__init__(self, size_hint=(None, None), size=(400, 400), pos=(500, 0))
+		layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+		for i in range(30):
+			btn = Button(text=str(i), size_hint_y=None, height=40)
+			layout.add_widget(btn)
+		self.add_widget(layout)
+
 class TouchtracerApp(App):
 	def build(self):
 		root = Widget()
 
 		# trying custom widget
 		painter = MyWidget()
-		with painter.canvas:
-			Color (1,1,1)
-			Line(points=(100,100,2000,2000))
 		root.add_widget(painter)
 
-		# trying button
-		clrbtn = Button(text='Reset')
-		root.add_widget(clrbtn)
-		def reset_canvas(obj):
-			painter.canvas.clear()
-			with painter.canvas:
-				Color (1,1,1)
-				Line(points=(100,100,2000,2000))
-		clrbtn.bind(on_release=reset_canvas)
-
-		# trying scatter 
-		picture = Scatter(do_rotation=False, do_scale=True, do_translation_y=False, rotation=0)
-		picture.add_widget(Image(source='img/dome.jpg'))
-		root.add_widget(picture)
-
 		# trying nested objects
-		window = Scatter(pos=(200,200), size=(300,300))
-		layout = BoxLayout(pos=(50, 50), size=(200,200))
-		btn1 = Button(text='Hello')
-		btn2 = Button(text='World')
-		layout.add_widget(btn1)
-		layout.add_widget(btn2)
-		window.add_widget(Image(source='img/background.jpg', size=(300,300)))
-		window.add_widget(layout)
-		root.add_widget(window)
+		window = Window()
+		# root.add_widget(window)
+
+		# trying workspace idea
+		workspace = Workspace()
+		root.add_widget(workspace)
 
 		return root
 
