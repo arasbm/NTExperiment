46ffe60e082bb76d1ed291b29b57616dc35c5c40
Amir Hossein Hajizadeh <amirhos.hajiz@gmail.com>
2011-07-25 22:54:51 -0700

    size doubled

--- neartouch.py
+++ neartouch.py
@@ -173,8 +173,8 @@ class Container(Scatter):
 
 	# returns a random value for size
 	def random_size(self):
-		base = 70
-		step = 30
+		base = 150
+		step = 40
 		max_levels = 3
 		return base+step*int(max_levels*random())
 
