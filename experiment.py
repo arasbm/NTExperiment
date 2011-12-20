5c6a3c7d36980145283e5ceb42eafe11f8bb49d5
Aras Balali Moghaddam <arasbm@uvic.ca>
2011-12-20 01:30:35 -0800

    add scale

--- experiment.py
+++ experiment.py
@@ -1,7 +1,7 @@
 import kivy
 from kivy.logger import Logger
 
-kivy.require('1.0.8') # replace with your current kivy version !
+kivy.require('1.0.7') # replace with your current kivy version !
 
 from kivy.app import App
 
@@ -28,8 +28,13 @@ grab_high_color = Color(0.86, 0.28, 0.078)
 release_low_color = Color(0.0,0.0, 0.7, 0.5)
 release_high_color = Color(0.86, 0.28, 0.078)
 
+screen_width = 1920
+screen_height = 1080
+record_number = 0
+vertical_border = 90 #1080 / 12
+horizontal_border = 160 #1920 / 12
 min_obj_width = 10
-max_obj_width = 200
+max_obj_width = 190 #actual maximum will be min_obj_width + max_obj_width
 
 class Object(Widget):
     def __init__(self, gesture):
@@ -45,8 +50,14 @@ class Object(Widget):
         #self.size=(50,50)
         #self.pos=(250,300)
     
+    #give a new random size to this object
+    def scale(self):
+        d = min_obj_width + round(max_obj_width*random())
+        self.img.size=(d, d)
+    
+    #move this obj to a new random location
     def move(self):
-        self.img.pos=(100+round(1400*random()), 100+round(700*random()))
+        self.img.pos=(horizontal_border+round((screen_width - 2 * horizontal_border)*random()), vertical_border+round((screen_height - 2 * vertical_border)*random()))
         #self.img.pos_hint=(10,10)
 
     def set_color(self, color):
@@ -70,15 +81,17 @@ class Root(Widget):
         if not 'markerid' in touch.profile:
             self.next_trial()
             return
-    hand_id = int(touch.fid / self.hand_gesture_offset)
-    gesture_id = touch.fid % self.hand_gesture_offset
-    if gesture_id == self.grab_gesture:
-        self.grab_obj.set_color((0.7,0.5,0.3,0.3))
-        self.release_obj.set_color((0.6,0.2,0.4,0.9))
-    if gesture_id == self.release_gesture:
-        self.next_trial()
+        hand_id = int(touch.fid / self.hand_gesture_offset)
+        gesture_id = touch.fid % self.hand_gesture_offset
+        if gesture_id == self.grab_gesture:
+            self.grab_obj.set_color((0.7,0.5,0.3,0.3))
+            self.release_obj.set_color((0.6,0.2,0.4,0.9))
+        if gesture_id == self.release_gesture:
+            self.next_trial()
 
     def next_trial(self):
+        self.grab_obj.scale()
+        self.release_obj.scale()
         self.grab_obj.move()
         self.grab_obj.set_color((0.7,0.5,0.3,0.9))
         self.release_obj.move()
